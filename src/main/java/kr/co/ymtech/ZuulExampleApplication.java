5da9d12af2067b849d747d74a8abdd2c4d9342ea
MJ Youn <yun0244@naver.com>
2019-05-27 14:14:48 +0900

    init

---
+++ src/main/java/kr/co/ymtech/ZuulExampleApplication.java
@@ -0,0 +1,17 @@
+package kr.co.ymtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulExampleApplication.class, args);
+	}
+
+}
+
+
